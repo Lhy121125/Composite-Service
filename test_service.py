3be5503c5cab2c064951aff6b66a271af3f6ece8
Lhy121125 <hl3648@columbia.edu>
2023-12-17 03:27:05 +0000

    remove depreciated method

--- test_service.py
+++ test_service.py
@@ -11,8 +11,8 @@ class AppTest(unittest.TestCase):
     def test_async_dashboard_data(self):
         # Test Async
         response = self.app.get('/dashboard/0')
-        self.assertEquals(response.status_code, 200)
-    
+        self.assertEqual(response.status_code, 200)
+        
     def test_sync_user_detail(self):
         # Test the sync
         response = self.app.get('/get-user-details/0')
